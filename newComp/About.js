092e9292a7183714e5795130c241c5ae2d03ab28
ZAIN-BUKHARI <zainnnnnnnnnnnnnnnnnnnn@gmail.com>
2023-08-12 20:27:23 +0500

    final commit for resposnive footer margin set

--- newComp/About.js
+++ newComp/About.js
@@ -1,26 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <>
-     <section id="about">
-    <div class="about container">
-      <div class="col-left">
-        <div class="about-img MEDIA-LEFT-ABOUT">
-          <img src="two.png" alt="img"/>
+      <section id="about">
+        <div class="about container">
+          <div class="col-left">
+            <div class="about-img MEDIA-LEFT-ABOUT">
+              <img src="two.png" alt="img" />
+            </div>
+          </div>
+          <div class="col-right">
+            <h1 class="section-title">
+              About <span>us</span>
+            </h1>
+            <h2>ghory trade</h2>
+            <p className="Service-Media-P">
+              We are a dynamic trading company specializing in the exchange of
+              commodities that drive global markets - oil, gold, and Bitcoin.
+              With a deep understanding of the intricacies of these diverse
+              assets, we navigate the ever-evolving landscape of international
+              trade, utilizing advanced strategies and technologies to seize
+              opportunities and manage risks effectively. Our commitment to
+              integrity, innovation, and maximizing value for our partners
+              defines our approach, making us a trusted player in the complex
+              world of commodity trading.
+            </p>
+            <a href="#" class="cta">
+              Join us
+            </a>
+          </div>
         </div>
-      </div>
-      <div class="col-right">
-        <h1 class="section-title">About <span>us</span></h1>
-        <h2>ghory trade</h2>
-        <p className='Service-Media-P'>
-We are a dynamic trading company specializing in the exchange of commodities that drive global markets - oil, gold, and Bitcoin. With a deep understanding of the intricacies of these diverse assets, we navigate the ever-evolving landscape of international trade, utilizing advanced strategies and technologies to seize opportunities and manage risks effectively. Our commitment to integrity, innovation, and maximizing value for our partners defines our approach, making us a trusted player in the complex world of commodity trading.</p>
-        <a href="#" class="cta">Join us</a>
-      </div>
-    </div>
-  </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
