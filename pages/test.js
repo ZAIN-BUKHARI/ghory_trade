20abd7e78904b10cb4fe5fc80ea9277e07b6354a
ZAIN-BUKHARI <zainnnnnnnnnnnnnnnnnnnn@gmail.com>
2023-08-18 12:05:18 +0500

    add modal beacuse of that going to change in babel rc

--- pages/test.js
+++ pages/test.js
@@ -1,51 +1,11 @@
-import React, { useState } from 'react';
-
+import React from 'react'
+import FormModal from '../universe.io/FormModal'
 const test = () => {
-  const [comment, setComment] = useState('');
-
-  const handleCommentChange = (event) => {
-    setComment(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const url = `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&key=AIzaSyC-OBV6dPSaRXbZ1nKUP5x48K34CiH3zrE`;
-    const requestBody = {
-      snippet: {
-        videoId: "YPdWRRLyDBg",
-        topLevelComment: {
-          snippet: {
-            textOriginal: "comment",
-          },
-        },
-      },
-    };
-
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody),
-      });
-
-      if (response.ok) {
-        console.log('Comment posted successfully!');
-      } else {
-        console.error('Failed to post comment');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
   return (
-    <div>
-        <button onClick={handleSubmit} type="submit">Post Comment</button>
-    </div>
-  );
-};
+   <>
+   <FormModal/>
+   </>
+  )
+}
 
-export default test;
+export default test
