198b81d53f3905739eb31b77a905547d2e31e538
ZAIN-BUKHARI <zainnnnnnnnnnnnnnnnnnnn@gmail.com>
2023-08-25 17:16:23 +0500

    responvie test start from here

--- Responsiveness/Header.js
+++ Responsiveness/Header.js
@@ -5,70 +5,70 @@ const Header = () => {
     <>
     <style>
         {`
-        // .mobile-main{
-        //     height:80px;
-        //     display:flex;
-        //     flex-direction:column;
-        //     justify-content:space-between;
+        .mobile-main{
+            height:80px;
+            display:flex;
+            flex-direction:column;
+            justify-content:space-between;
 
-        // }
-        // .div-one{
-        //     justify-content:space-between;
-        // }
-        // .div-two{
-        //     justify-content:center;
+        }
+        .div-one{
+            justify-content:space-between;
+        }
+        .div-two{
+            justify-content:center;
             
-        // }
-        // .list1{
-        //     margin-right:15px
-        // }
-        // .list2{
-        //     margin-right:15px
-        // }
-        // .list3{
-        //     margin-right:15px
-        // }
-        // .list4{
-        //     margin-right:15px
-        // }
-        // .list5{
-        //     margin-right:15px
-        // }
+        }
+        .list1{
+            margin-right:15px
+        }
+        .list2{
+            margin-right:15px
+        }
+        .list3{
+            margin-right:15px
+        }
+        .list4{
+            margin-right:15px
+        }
+        .list5{
+            margin-right:15px
+        }
         
-        // li{
-        //     font-size:12px;
-        //     font-family:serif;
-        //     font-weight:bold;
-        // }
+        .mobile-list {
+            font-size:12px;
+            font-family:serif;
+            font-weight:bold;
+        }
         // h1{
         //     font-size:12px;
         //     font-family:serif;
         //     font-weight:bold;
         // }
-        // img{
-        //     height:50px;
-        //     width:70px;
-        // }
-        // .hamburger{
-        //     margin-right:10px;
-        //     margin-top:10px;
-        //     font-size:20px;
-        // }
+        .mobile-img{
+            height:50px;
+            width:70px;
+        }
+        .hamburger{
+            margin-right:10px;
+            margin-top:10px;
+            font-size:20px;
+        }
         `}
     </style>
     <div className='mobile-main'>
         <div className=' flex div-one'>
             {/* <h1>Ghory.trading</h1> */}
-            <img src='remove_bg.png'/>
+            <img className='mobile-img' src='remove_bg.png'/>
             <FcMenu className='hamburger'/>
         </div>
         <div className=''>
             <ul className='flex div-two'>
-                <li className='list1'>Home</li>
-                <li className='list2'>About</li>
-                <li className='list3'>Project</li>
-                <li className='list4'>Contact</li>
-                <li className='list5'>invest</li>
+                <li className='list1 mobile-list'>Home</li>
+                <li className='list2 mobile-list'>About</li>
+                <li className='list3 mobile-list'>Project</li>
+                <li className='list4 mobile-list'>Contact</li>
+                <li className='list5 mobile-list'>invest</li>
             </ul>
         </div>
     </div>
