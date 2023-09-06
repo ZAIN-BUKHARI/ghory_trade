237a103457065d5e1c0fce088868f09c85f0a17b
ZAIN-BUKHARI <zainnnnnnnnnnnnnnnnnnnn@gmail.com>
2023-09-06 08:15:34 +0500

    remove cron for vercel build fail

--- pages/api/TTL/cron.js
+++ pages/api/TTL/cron.js
@@ -1,6 +1,6 @@
-// pages/api/scheduleCleanup.js
-const cron = require('node-cron');
-const cleanupDocuments = require('../../scripts/documentCleanup');
+// // pages/api/scheduleCleanup.js
+// const cron = require('node-cron');
+// const cleanupDocuments = require('../../scripts/documentCleanup');
 
-// Schedule the cleanup job to run every day at a specific time (e.g., midnight)
-cron.schedule('0 0 * * *', cleanupDocuments);
+// // Schedule the cleanup job to run every day at a specific time (e.g., midnight)
+// cron.schedule('0 0 * * *', cleanupDocuments);
