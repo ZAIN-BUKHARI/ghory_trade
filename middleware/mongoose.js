bb6803f1fe58f253843ccd4edecfe7c0fbcf7d7f
ZAIN-BUKHARI <zainnnnnnnnnnnnnnnnnnnn@gmail.com>
2023-09-23 22:14:45 +0500

    bacnk to zain base

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -8,8 +8,8 @@ const ConnectMongoDB = handler=> async (req,res) =>{
     
         // await mongoose.connect('mongodb+srv://usmanghory3:WATCHDOGS426890@ghory-trading.rxugb1n.mongodb.net/GHORYTRADE=true&w=majority').then(res=>{
             // mongodb+srv://usmanghory3:WATCHDOGS426890@ghory-trading.rxugb1n.mongodb.net/?retryWrites=true&w=majority
-        // await mongoose.connect('mongodb+srv://123:123@e-store.uf5qztz.mongodb.net/ZAINYWEAR=true&w=majority').then(res=>{
-        await mongoose.connect('mongodb+srv://usmanghory3:WATCHDOGS426890@ghory-trading.rxugb1n.mongodb.net/GHORYTRADE=true&w=majority').then(res=>{
+        await mongoose.connect('mongodb+srv://123:123@e-store.uf5qztz.mongodb.net/ZAINYWEAR=true&w=majority').then(res=>{
+        // await mongoose.connect('mongodb+srv://usmanghory3:WATCHDOGS426890@ghory-trading.rxugb1n.mongodb.net/GHORYTRADE=true&w=majority').then(res=>{
             console.log('connect',res)
         })
         return handler(req,res)
