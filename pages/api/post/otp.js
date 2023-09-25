1cd04b9176ba5f856e19721a4b6dff88207e0843
ZAIN-BUKHARI <zainnnnnnnnnnnnnnnnnnnn@gmail.com>
2023-09-25 11:43:15 +0500

    nodemailer solution test

--- pages/api/post/otp.js
+++ pages/api/post/otp.js
@@ -8,19 +8,50 @@ const handler= async (req, res)=> {
        a = a.toString().slice(2,6)
        try{ 
         let transporter = nodemailer.createTransport({
+            port: 465,
+            host: "smtp.gmail.com",
             service: 'Gmail',
               auth: {
               user: `${process.env.NODE_MAILER_USER}`, 
               pass: `${process.env.NODE_MAILER_PASS}`
-                    }
+                    },
+             secure: true,
+
         });
-        let mailOptions = {
+
+        await new Promise((resolve, reject) => {
+                    // verify connection configuration
+                    transporter.verify(function (error, success) {
+                        if (error) {
+                            console.log(error);
+                            reject(error);
+                        } else {
+                            console.log("Server is ready to take our messages");
+                            resolve(success);
+                        }
+                    });
+                });
+
+
+        let mailData = {
             from: `${process.env.NODE_MAILER_USER}`, 
             to: `${email}`,
         subject: 'GHORY.TRADE',
         text: `OTP CODE FOR EMAIL VERIFICATION ${a}
         `
         };
+            await new Promise((resolve, reject) => {
+        // send mail
+        transporter.sendMail(mailData, (err, info) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+             res.status(200).json({otp:a,success:true})
+                resolve(info);
+            }
+        });
+    });
         transporter.sendMail(mailOptions).then(result=>{
             try{
             }catch(e){
@@ -42,3 +73,35 @@ else{
   
   
   export default   ConnectMongoDB(handler)
+
+
+    
+
+    
+
+    
+//     const mailData = {
+//         from: {
+//             name: `${firstName} ${lastName}`,
+//             address: "myEmail@gmail.com",
+//         },
+//         replyTo: email,
+//         to: "recipient@gmail.com",
+//         subject: `form message`,
+//         text: message,
+//         html: `${message}`,
+//     };
+    
+//     await new Promise((resolve, reject) => {
+//         // send mail
+//         transporter.sendMail(mailData, (err, info) => {
+//             if (err) {
+//                 console.error(err);
+//                 reject(err);
+//             } else {
+//                 console.log(info);
+//                 resolve(info);
+//             }
+//         });
+//     });
+    
