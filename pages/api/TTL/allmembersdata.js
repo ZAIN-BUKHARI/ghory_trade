efd95d7710553d363d12ec8e463d4acc991a598b
ZAIN-BUKHARI <zainnnnnnnnnnnnnnnnnnnn@gmail.com>
2023-10-06 11:22:55 +0500

    id!=no

--- pages/api/TTL/allmembersdata.js
+++ pages/api/TTL/allmembersdata.js
@@ -3,18 +3,21 @@ import User from '../../../models/User'
 
 const handler= async (req, res)=> {
     if(req.method=='POST'){
+        console.log('hittttttttttttttttttttt')
+        console.log(req.body.email)
         let user = await User.findOne({email:req.body.email})
+        console.log(user)
         let members = []
         for(let i=0;i<user.teams.length;i++)
         {   
             let direct = user.teams[i]['direct'].id
             let indirect = user.teams[i]['indirect'].id
-            if(user.teams[i]['direct'].id!='')
+            if(user.teams[i]['direct'].id!='no')
             {
                 let u = await User.findOne({_id:direct})
                 members.push(u)
             }
-            if(user.teams[i]['indirect'].id!='')
+            if(user.teams[i]['indirect'].id!='no')
             {
                 let u = await User.findOne({_id:indirect})
                 members.push(u)
